32c935b1c077c5cd404eb6dc0a358356a92aced9
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-18 20:01:40 +0530

    Data Types In JS

---
+++ JAVASCRIPT/1.Basics/bootcamp.js
@@ -0,0 +1,23 @@
+//string
+
+let customerName = "Deepak Roy";
+
+//number
+
+let age = 20;
+
+//boolean
+
+let isPaid = true ;
+
+//null
+
+let favouriteClass = null;
+
+//unsigned
+
+let hometown ;
+
+console.log(hometown) ;
+
+console.log(typeof(favouriteClass)) ; 
