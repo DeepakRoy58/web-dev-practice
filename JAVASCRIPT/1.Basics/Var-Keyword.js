4a0371892224c404ba61a51be9172b82964bac2e
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-20 04:18:22 +0530

    As the key learning was var is an keyword in which its an global and fuunction scoped as its not a block-scoped ,it can be performed as Reassignment and Redefinition

---
+++ JAVASCRIPT/1.Basics/Var-Keyword.js
@@ -0,0 +1,23 @@
+// VAR IS A  FUNCTION SCOPED AND GLOBAL SCOPED , ITS NOT AN BLOCK - SCOPED
+
+//REASSIGNMENT 
+
+var x =10 ;
+
+console.log(x);
+
+x = "Mayank" ;
+
+console.log(x);
+
+//Redefinition
+
+var x =98 ;
+
+console.log(x);
+
+var x = "Deepak"
+
+console.log(x);
+
+
