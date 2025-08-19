3f1d60c15415d094f5a51ef48ccedb11fe60e57b
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-20 05:05:20 +0530

    block-scope

--- JAVASCRIPT/1.Basics/Var-Keyword.js
+++ JAVASCRIPT/1.Basics/Var-Keyword.js
@@ -20,4 +20,20 @@ var x = "Deepak"
 
 console.log(x);
 
+//  Whats Block - scoped ???
+
+// "Block scope" (or blocked scope as you wrote) means that
+//  a variable declared inside a block of code (enclosed by { })
+//   is only accessible within that block - and not outside it.
+
+
+{
+    let a = 10;
+    const pi  = 3.14;
+    var c = "Deepak Roy" ;
+}
+
+console.log(typeof a); //Reference error : as a is not defined
+console.log(typeof pi);//Reference error : as pi is not defined
+console.log(typeof c); // works because its an function-scope not an block-scope
 
