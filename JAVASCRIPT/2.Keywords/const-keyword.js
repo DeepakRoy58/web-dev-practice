e89ed5a84cbb94293ce5e556cf3fb7ea3f710007
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-20 21:14:49 +0530

    learnt keywords

---
+++ JAVASCRIPT/2.Keywords/const-keyword.js
@@ -0,0 +1,8 @@
+// In JavaScript, the const keyword is used to declare constants. Constants are variables
+// whose values cannot be reassigned once they are initialized.
+
+const x = 20;
+console.log(x);      // Output : 20
+
+x = 30;
+console.log(x);      // Output : Error!
---
+++ JAVASCRIPT/2.Keywords/let-keyword.js
@@ -0,0 +1,17 @@
+
+//Resassignment using let 
+
+let x = 10 ;
+console.log(x);
+
+x ="Deepak" ;
+console.log(x);
+
+
+//Redefinition using let
+
+// let y = 10 ;
+// let y = "Deepak";
+
+// console.log(y); // output : error
+
