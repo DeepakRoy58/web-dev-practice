1a880c505c4148e5c163af42e99ea2c2b8917b6c
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-22 08:11:03 +0530

    primitive data types in js

---
+++ JAVASCRIPT/3.DataTypes/primitive.js
@@ -0,0 +1,25 @@
+//string
+
+let customerName = "Deepak Roy";
+
+//number
+
+let age = 20;
+
+//boolean
+
+let isPaid = true ;
+
+//null
+
+let favouriteClass = null;
+
+//unsigned
+
+let hometown ;
+
+console.log(typeof(customerName));
+console.log(typeof(age));
+console.log(typeof(isPaid));
+console.log(typeof(favouriteClass));
+console.log(typeof(hometown));
