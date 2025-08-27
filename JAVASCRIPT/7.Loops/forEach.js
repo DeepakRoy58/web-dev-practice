7f1a434ab0a2735f4bdbcf558cc9c3bea7cfe190
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-27 23:41:18 +0530

    for each

---
+++ JAVASCRIPT/7.Loops/forEach.js
@@ -0,0 +1,16 @@
+
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+
+numbers.forEach(function(number, index){
+    console.log(`Element at ${index} : ${number}`)
+})
+
+/* Output :
+Element at 0 : 1
+Element at 1 : 2
+Element at 2 : 3
+Element at 3 : 4
+Element at 4 : 5
+Element at 5 : 6
+Element at 6 : 7
+*/
