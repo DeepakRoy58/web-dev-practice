8a115d54d87aaa84dc5c69fd68766d6f50e23f3b
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-28 19:48:47 +0530

    JavaScript Features

---
+++ JAVASCRIPT/8.JavaScript Features/StaticVsDynamic.js
@@ -0,0 +1,20 @@
+/* Static Typing : 
+
+int marks = 95;
+
+char alpha = 'm';
+
+string name = "Love Babbar"; */
+
+/* Dynamic Typing : */
+let marks = 95;
+console.log(typeof(marks));  //number
+
+marks = "Namaste dunia";
+console.log(typeof(marks));  //string
+
+marks = 'a';
+console.log(typeof(marks));  //string
+
+marks = true;
+console.log(typeof(marks));  //boolean
