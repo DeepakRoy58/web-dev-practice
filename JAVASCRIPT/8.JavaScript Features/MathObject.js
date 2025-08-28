3f1cabccb80abdee6b09aebfb32a56ccd60b3de4
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-08-28 19:53:12 +0530

    Math Object In JS

---
+++ JAVASCRIPT/8.JavaScript Features/MathObject.js
@@ -0,0 +1,21 @@
+/* Math.PI */
+
+let value = Math.PI;
+console.log(value);
+
+//Output : 3.141592653589793
+
+/* Math.E */
+
+let val = Math.E;
+console.log(value);
+
+//Output : 2.718281828459045
+
+// Math.LN2
+
+let value1 = Math.LN2;
+console.log(value1);
+
+//Output : 0.6931471805599453
+
