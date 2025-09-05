8051d7cbe3680e9162c9665f36647d82e7996322
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-09-05 23:31:33 +0530

    Strings In JS

--- 3.JAVASCRIPT/10.Strings/learn.js
+++ 3.JAVASCRIPT/10.Strings/learn.js
@@ -1,3 +1,22 @@
 let name = " Deepak Roy" ;
 console.log(name);
 console.log(typeof(name));
+
+let firstName = new String("Durga Rajesh") ;
+console.log(firstName);
+
+let op1 = "Learning JS" ;
+let op2 = "From Documentation" ;
+
+/* let ans = op1 + op2;
+console.log(ans); */
+
+/* let finalAns = `${op1} ${op2}` ;
+console.log(finalAns);
+ */
+
+console.log(op2.length);
+console.log(op1.toUpperCase());
+console.log(op2.toLowerCase());
+
+
