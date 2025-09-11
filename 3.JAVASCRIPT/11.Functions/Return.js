a63c9aeb6e6ce7094e8f50863dcc04b71d002792
DeepakRoy58 <pylalakshmideepak9394@gmail.com>
2025-09-11 17:52:03 +0530

    Function Expression

--- 3.JAVASCRIPT/11.Functions/Return.js
+++ 3.JAVASCRIPT/11.Functions/Return.js
@@ -16,3 +16,13 @@ function userName(firstName, lastName) {
 
 let fullName = userName("Roy" , "Deepak") ;
 console.log("user name was : " ,fullName);
+
+
+/* Function Expression */
+
+let getExpo = function (x,y) {
+    let Expo = x**y ;
+    return Expo ;    
+}
+
+console.log(getExpo(2,10));
